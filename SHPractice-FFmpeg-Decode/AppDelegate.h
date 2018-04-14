d453031215ee26f701ef766a5cb107c73dbbcd17
wangxiang <wangxiang@yixia.com>
2018-04-14 22:05:16 +0800

    Initial Commit

---
+++ SHPractice-FFmpeg-Decode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SHPractice-FFmpeg-Decode
+//
+//  Created by Shine on 14/04/2018.
+//  Copyright © 2018 shine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SHPractice-FFmpeg-Decode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SHPractice-FFmpeg-Decode
+//
+//  Created by Shine on 14/04/2018.
+//  Copyright © 2018 shine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
